5e847b10457b4442027eaa86f35c8318d4bcb5be
juliana <julianavido@outlook.com>
2023-06-16 18:01:43 -0700

    adjusts

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,23 @@
 import { Route, Routes } from "react-router-dom";
-import { AuthRouteProtector } from "./components/AuthRouteProtector";
-import { AuthContextComponent } from "./contexts/authContext";
 import { Error } from "./pages/Error";
 import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { Profile } from "./pages/Profile";
-import { Signup } from "./pages/Signup";
 import { Navbar } from "../src/components/Navbar";
 import { ServiceDetails } from "./pages/ServiceDetails";
 
 function App() {
   return (
     <>
-      <AuthContextComponent>
+      
       <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/coloracao" element={<ServiceDetails id={1}/>} />
           <Route path="/descubra" element={<ServiceDetails id={2}/>} />
           <Route path="/closet" element={<ServiceDetails id={3}/>} />
-          <Route path="/consultoria-completa" element={<ServiceDetails id={4}/>} />
-
-
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/profile"
-            element={<AuthRouteProtector component={Profile} />}
-          />
-          
+          <Route path="/consultoria-completa" element={<ServiceDetails id={4}/>} />      
           <Route path="*" element={<Error />} />
         </Routes>
-      </AuthContextComponent>
+      
     </>
   );
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,23 +1,11 @@
 import Logo from "../../assets/images/logo.png"
 import Bag from "../../assets/images/bag.png"
-import { useContext } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { AuthContext } from "../../contexts/authContext";
-//import { ConfirmationButton } from "../ConfirmationButton/index";
+import { Link  } from "react-router-dom";
+
 
 
 export function Navbar() {
-  const { loggedInUser, setLoggedInUser } = useContext(AuthContext);
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  function handleLogout() {
-    setLoggedInUser("");
-    localStorage.removeItem("loggedInUser");
-    navigate("/");
-  }
-
-  return (
+    return (
     <>
       <div className="mx-auto flex items-center justify-around  lg:px-8 quad-background h-auto">
       <Link to="/">
